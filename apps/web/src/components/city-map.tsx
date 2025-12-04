250422272987608c5aa660442cae0f190246b726
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2025-12-04 21:54:34 +0100

    feat: improve styling and ux of map markers

--- apps/web/src/components/city-map.tsx
+++ apps/web/src/components/city-map.tsx
@@ -1,38 +1,109 @@
 import { useQuery } from '@tanstack/react-query';
-import { Map, Marker as MapMarker } from 'mapbox-gl';
-import { useEffect, useRef, useState, type RefObject } from 'react';
+import { Map, Marker as MapMarker, Popup } from 'mapbox-gl';
+import {
+  useEffect,
+  useEffectEvent,
+  useRef,
+  useState,
+  type RefObject,
+} from 'react';
 import z from 'zod';
 
 import { stopSchema } from '../schemas/gtfs';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { cn } from '../lib/utils';
+import { BusFront } from 'lucide-react';
 
 const Marker = ({
   stop,
   mapRef,
+  active = false,
+  onClick,
 }: {
   stop: z.infer<typeof stopSchema>;
   mapRef: RefObject<Map>;
+  active?: boolean;
+  onClick?: () => void;
 }) => {
-  const markerRef = useRef<MapMarker>();
-  const el = useRef<HTMLDivElement>();
+  const markerRef = useRef<MapMarker>(null);
+  const markerContainerRef = useRef<HTMLDivElement>(null);
+  const popupRef = useRef<Popup>(null);
+  const hoverTimeoutRef = useRef<ReturnType<typeof setTimeout>>(null);
 
   useEffect(() => {
-    markerRef.current = new MapMarker(el.current)
-      .setLngLat({
-        lon: stop.stop_lon,
-        lat: stop.stop_lat,
-      })
+    if (!mapRef.current || !markerContainerRef.current) return;
+
+    const marker = new MapMarker({
+      anchor: 'center',
+      element: markerContainerRef.current,
+    })
+      .setLngLat([stop.stop_lon, stop.stop_lat])
       .addTo(mapRef.current);
+
+    markerRef.current = marker;
+
+    const popup = new Popup({
+      anchor: 'top',
+      offset: 20,
+      closeButton: false,
+      closeOnClick: false,
+    })
+      .setLngLat([stop.stop_lon, stop.stop_lat])
+      .setText(stop.stop_name);
+
+    popupRef.current = popup;
+
+    return () => {
+      marker.remove();
+      popup.remove();
+    };
   }, []);
 
+  const handleMouseEnter = () => {
+    if (hoverTimeoutRef.current != null) clearTimeout(hoverTimeoutRef.current);
+
+    hoverTimeoutRef.current = setTimeout(() => {
+      if (mapRef.current && popupRef.current) {
+        popupRef.current.addTo(mapRef.current);
+      }
+    }, 300);
+  };
+
+  const handleMouseLeave = () => {
+    if (hoverTimeoutRef.current) {
+      clearTimeout(hoverTimeoutRef.current);
+      hoverTimeoutRef.current = null;
+    }
+
+    if (!active) {
+      popupRef.current?.remove();
+    }
+  };
+
+  useEffect(() => {
+    if (active) {
+      popupRef.current?.addClassName('mapboxgl-popup-active');
+    }
+  }, [active]);
+
   return (
-    <div
-      ref={el}
-      className="group relative h-4 w-4 cursor-pointer rounded-full border-2 border-yellow-700 bg-red-900 transition-colors hover:border-yellow-500 hover:bg-red-800"
-    >
-      <span className="invisible absolute top-5 left-1/2 -translate-x-1/2 rounded-sm bg-black/30 p-1 text-xs font-medium text-white transition-all delay-200 group-hover:visible">
-        {stop.stop_name}
-      </span>
+    <div ref={markerContainerRef} className="marker-mapbox-anchor">
+      <div
+        className={cn(
+          'group relative flex cursor-pointer items-center justify-center rounded-full border-2 transition-all duration-200',
+          active
+            ? 'h-8 w-8 border-yellow-500 bg-yellow-500 shadow-lg shadow-yellow-400/50 hover:bg-yellow-500'
+            : 'h-4 w-4 border-yellow-700 bg-amber-900 hover:border-yellow-500 hover:bg-red-800',
+        )}
+        onClick={onClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <BusFront
+          size={20}
+          className={cn(active ? 'visible scale-100' : 'invisible scale-75')}
+        />
+      </div>
     </div>
   );
 };
@@ -40,7 +111,8 @@ const Marker = ({
 export const CityMap = () => {
   const mapRef = useRef<Map>(null);
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  const [activeStopId, setActiveStopId] = useState<string | null>();
+  const [startStopId, setStartStopId] = useState<string | null>();
+  const [endStopId, setEndStopId] = useState<string | null>();
 
   const { data: stops } = useQuery({
     queryKey: ['stops'],
@@ -53,6 +125,14 @@ export const CityMap = () => {
       return parsedData;
     },
   });
+
+  const handlePickStops = (stopId: string) => {
+    if (startStopId == null) {
+      setStartStopId(stopId);
+    } else if (endStopId == null && stopId !== startStopId) {
+      setEndStopId(stopId);
+    }
+  };
 
   useEffect(() => {
     if (!mapContainerRef.current) return;
@@ -84,7 +164,15 @@ export const CityMap = () => {
       ref={mapContainerRef}
     >
       {mapRef.current &&
-        stops.map((s) => <Marker key={s.stop_id} stop={s} mapRef={mapRef} />)}
+        stops.map((s) => (
+          <Marker
+            key={s.stop_id}
+            stop={s}
+            mapRef={mapRef}
+            active={s.stop_id === startStopId || s.stop_id === endStopId}
+            onClick={() => handlePickStops(s.stop_id)}
+          />
+        ))}
     </div>
   );
 };
